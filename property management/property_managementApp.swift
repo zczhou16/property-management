2e5c998740326b596a4dd73e1de000588a19d18d
zczhou16 <zczhou16@gmail.com>
2023-05-05 15:38:09 -0700

    Initial Commit

---
+++ property management/property_managementApp.swift
@@ -0,0 +1,20 @@
+//
+//  property_managementApp.swift
+//  property management
+//
+//  Created by Zichao's Mac2023 on 2023-05-05.
+//
+
+import SwiftUI
+
+@main
+struct property_managementApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ property managementUITests/property_managementUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  property_managementUITestsLaunchTests.swift
+//  property managementUITests
+//
+//  Created by Zichao's Mac2023 on 2023-05-05.
+//
+
+import XCTest
+
+final class property_managementUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
